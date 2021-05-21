e8bebe6fde4c5bc7c8884df82593b2315b2aa32f
riski_putra <riski.putra@stickearn.com>
2021-05-22 02:29:29 +0700

    Stack navigator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {GetStarted} from './pages';
+import Router from './router';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <GetStarted />
+      <Router />
     </NavigationContainer>
   );
 }
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {GetStarted, Splash} from '../pages';
+
+const Stack = createStackNavigator();
+const Router = () => {
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="GetStarted"
+        component={GetStarted}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
