5225739c0db5b7db86ed195edf7ebae273e8bbae
riski_putra <riski.putra@stickearn.com>
2021-05-29 06:05:09 +0700

    upload photo

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {GetStarted, Login, Register, Splash} from '../pages';
+import {GetStarted, Login, Register, Splash, UploadPhoto} from '../pages';
 
 const Stack = createStackNavigator();
 const Router = () => {
@@ -24,6 +24,11 @@ const Router = () => {
       <Stack.Screen
         name="Login"
         component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="UploadPhoto"
+        component={UploadPhoto}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
