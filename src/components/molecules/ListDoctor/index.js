b23997e297b0c9a71241645dd87b45e79e85a057
riski_putra <riski.putra@stickearn.com>
2021-05-30 21:21:31 +0700

    doctor - messages

---
+++ src/components/molecules/ListDoctor/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {DummyDoctor2} from '../../../assets';
+import {colors, fonts} from '../../../utils';
+
+const ListDoctor = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={DummyDoctor2} style={styles.avatar} />
+      <View style={styles.content}>
+        <Text style={styles.name}>Alexander jannie</Text>
+        <Text style={styles.desc}>
+          Baik ibu, Terimakasi banyak atas waktu...
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ListDoctor;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  content: {flex: 1, marginLeft: 16},
+  avatar: {width: 46, height: 46, borderRadius: 46 / 2},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.primary,
+  },
+  desc: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.secondary,
+    textTransform: 'capitalize',
+  },
+});
--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import ListDoctor from '../../components/molecules/ListDoctor';
+import {colors, fonts} from '../../utils';
 
 const Messages = () => {
   return (
-    <View>
-      <Text>Messages Page</Text>
+    <View style={styles.page}>
+      <Text>Messages</Text>
+      <ListDoctor />
+      <ListDoctor />
+      <ListDoctor />
     </View>
   );
 };
 
 export default Messages;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {backgroundColor: colors.secondary, flex: 1},
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginLeft: 16,
+  },
+});
