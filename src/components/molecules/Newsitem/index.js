32a45af3c8d93822e2bedaff31862df55457b31d
riski_putra <riski.putra@stickearn.com>
2021-05-30 18:48:08 +0700

    doctor - userProfile

---
+++ src/components/molecules/Newsitem/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Newsitem = () => {
+  return (
+    <View>
+      <Text>Newsitem</Text>
+    </View>
+  );
+};
+
+export default Newsitem;
+
+const styles = StyleSheet.create({});
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,4 +1,15 @@
 import BottomNavigator from './BottomNavigator';
+import DoctorCategory from './DoctorCategory';
 import Header from './Header';
+import HomeProfile from './HomeProfile';
+import Newsitem from './Newsitem';
+import RatedDoctor from './RatedDoctor';
 
-export {Header, BottomNavigator};
+export {
+  Header,
+  BottomNavigator,
+  HomeProfile,
+  DoctorCategory,
+  RatedDoctor,
+  Newsitem,
+};
