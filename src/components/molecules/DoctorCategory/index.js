99d23c17374b5c1b2ac096a511d5fa6308755581
riski_putra <riski.putra@stickearn.com>
2021-05-30 19:16:23 +0700

    doctor - category

--- src/components/molecules/DoctorCategory/index.js
+++ src/components/molecules/DoctorCategory/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {ILCatUmum} from '../../../assets';
+import {colors, fonts} from '../../../utils';
 
 const DoctorCategory = () => {
   return (
-    <View>
-      <Text>DoctorCategory</Text>
+    <View style={styles.container}>
+      <ILCatUmum style={styles.ilustration} />
+      <Text style={styles.label}>Saya Butuh </Text>
+      <Text style={styles.category}>Dokter Umum</Text>
     </View>
   );
 };
 
 export default DoctorCategory;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    backgroundColor: colors.cardLight,
+    alignSelf: 'flex-start',
+    borderRadius: 10,
+    marginRight: 10,
+    width: 100,
+    height: 130,
+  },
+  ilustration: {
+    marginBottom: 28,
+  },
+  label: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.primary,
+  },
+  category: {
+    fontSize: 12,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+  },
+});
--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {
   DoctorCategory,
+  Gap,
   HomeProfile,
   Newsitem,
   RatedDoctor,
@@ -11,18 +12,30 @@ import {colors, fonts} from '../../utils';
 const Doctor = () => {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcome}>Mau konsultasi dengan siapa hari ini?</Text>
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <Text>Top Rated Doctors</Text>
-      <RatedDoctor />
-      <Text>Good News</Text>
-      <Newsitem />
-      <Newsitem />
-      <Newsitem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcome}>
+          Mau konsultasi dengan siapa hari ini?
+        </Text>
+        <View style={styles.wrapperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.category}>
+              <Gap width={16} />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <Gap width={6} />
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+        <RatedDoctor />
+        <Text style={styles.sectionLabel}>Good News</Text>
+        <Newsitem />
+        <Newsitem />
+        <Newsitem />
+      </View>
     </View>
   );
 };
@@ -33,6 +46,16 @@ const styles = StyleSheet.create({
   page: {
     paddingVertical: 30,
     paddingHorizontal: 16,
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    paddingVertical: 30,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    flex: 1,
   },
   welcome: {
     fontSize: 20,
@@ -41,5 +64,18 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 16,
     maxWidth: 209,
+  },
+  category: {
+    flexDirection: 'row',
+  },
+  wrapperScroll: {
+    marginHorizontal: -16,
+  },
+  sectionLabel: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
   },
 });
