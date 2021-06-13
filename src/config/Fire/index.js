3e5840c601ff3a04a5d92c5573d1e18054b88fba
riski_putra <riski.putra@stickearn.com>
2021-06-13 21:39:40 +0700

    setup firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyA4IVrqWjgTkbd6V12o6MjU2GW31twHGD0',
+  authDomain: 'mydoctor-c1332.firebaseapp.com',
+  projectId: 'mydoctor-c1332',
+  storageBucket: 'mydoctor-c1332.appspot.com',
+  messagingSenderId: '395752220311',
+  appId: '1:395752220311:web:58e547c2d38312c02e6bd2',
+});
+
+const Fire = firebase;
+
+export default Fire;
